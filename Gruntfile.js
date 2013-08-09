6dfbb27ab07178b3f88f8c95768ba7352ce3175e
shimar <shima01dev@gmail.com>
2013-08-09 18:43:53 +0900

    add a default Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+        options: {
+          prett: true
+        },
+        files: {
+          "dst/index.html": ["src/jade/index.jade"]
+        }
+      }
+    },
+    less: {
+      compile: {
+        options: {
+          compress: true,
+          syncimport: true
+        },
+        files: {
+          "dst/stylesheets/<%= pkg.name %>.min.css": "src/less/styles.less"
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        files: {
+          "dst/javascripts/<%= pkg.name %>.js": "src/coffee/*.coffee"
+        }
+      }
+    },
+    clean: {
+      build: ["dst"]
+    }
+  });
+
+  // load npm tasks.
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // define default tasks.
+  grunt.registerTask('default', ['clean', 'jade', 'less', 'coffee']);
+
+};
